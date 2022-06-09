de169dad01f38694dd54d6546ee6f7abed4cdd76
Oliver Smith <77962172+OliverPSmith@users.noreply.github.com>
2022-06-09 16:39:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 
 /************************** Menu Toggle */
+// Toggle menu onClick
+
 
 const hamburger = document.querySelector('.hamburger');
 const header = document.querySelector('.header');
@@ -9,6 +11,7 @@ const navLinks = Array.from(document.querySelectorAll('.nav-link'));
 const hero = document.querySelector('.hero');
 const body = document.querySelector('.body');
 
+//when menu icon is clicked toggle the 'active' class on these elements
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     header.classList.toggle('active');
@@ -16,6 +19,7 @@ hamburger.addEventListener('click', () => {
     hero.classList.toggle('active');
     body.classList.toggle('active');
 
+    //when nav links in the menu are clicked remove the 'active' class on these elements
     for (let i = 0; i < navLinks.length; i++) {
         navLinks[i].addEventListener('click', () => {
             hamburger.classList.remove('active')
@@ -28,6 +32,8 @@ hamburger.addEventListener('click', () => {
 });
 
 /************************** Scroll to highlight menu option */
+// Add the 'scroll-active' class to the menu nav links if they are in visible - onScroll
+
 
 const section = document.querySelectorAll('section');
 const menu = document.querySelectorAll('.nav-link');
@@ -51,19 +57,25 @@ window.onscroll = () => {
 
 
 /************************** Work Tab Toggle */
+// Toggle tab heading and its content - onClick
+
 
 const tabs = document.querySelectorAll('[data-tab-target]');
 const tabContents = document.querySelectorAll('[data-tab-content]');
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
+        // make the target (the tab clicked) a dataset variable
         const target = document.querySelector(tab.dataset.tabTarget)
+        // when tab is clicked remove the 'active' class for the tab content
         tabContents.forEach(tabContent => {
             tabContent.classList.remove('active');
         });
+        // remove 'active' class for the tab clicked
         tabs.forEach(tab => {
             tab.classList.remove('active');
         });
+        // add 'active' class for the tab clicked and its content
         tab.classList.add('active')
         target.classList.add('active');
     });
@@ -71,21 +83,27 @@ tabs.forEach(tab => {
 
 
 /************************** Scroll down to animate projects */
+// each project does small animation when coming into view by scrolling down
+
 
 const revealProject = () => {
+    // select all projects
     const projects = document.querySelectorAll('.project-img-container');
 
     for (let i = 0; i < projects.length; i++) {
+        // identify distance above project to start animation - 150px
         const windowHeight = window.innerHeight;
         const elementTop = projects[i].getBoundingClientRect().top;
         const elementVisible = 150;
 
+        // if element goes below 150px from top add the 'show' class (animation)
         if (elementTop < windowHeight - elementVisible) {
             projects[i].classList.add('show');
-            console.log('scrolling')
         }   else {
+            // if goes above then remove 'show' class
             projects[i].classList.remove('show');
         };
     };
 };
+// call the function
 window.addEventListener('scroll', revealProject);
